cb3cfdf5126ee2f62c8fed4d307c1bbd9b454f20
ChoiGYoung <choidh1261@naver.com>
2024-01-23 16:08:43 +0900

    수학 객체

---
+++ class/03-javascript/연습-07-math.js
@@ -0,0 +1,18 @@
+Math.floor(Math.random() * 100000)
+// 17209
+String(Math.floor(Math.random() * 100000))
+// '91721'
+String(Math.floor(Math.random() * 100000))
+// '56588'
+String(Math.floor(Math.random() * 100000))
+// '68775'
+String(Math.floor(Math.random() * 100000))
+// '62815'
+String(Math.floor(Math.random() * 100000)).padStart(6,"0")
+// '089748'
+String(Math.floor(Math.random() * 100000)).padStart(6,"0")
+// '053883'
+let result = String(Math.floor(Math.random() * 100000)).padStart(6,"0")
+// undefined
+result
+// '007833'
